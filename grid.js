3efa60ea71e645f84e379224748d240beb70d687
Anthony Bekhet <abekhet2023@gmail.com>
2025-11-15 17:23:49 -0500

    Basic Structure

---
+++ grid.js
@@ -0,0 +1,40 @@
+const gameGrid = document.getElementById("gameGrid");
+
+class gridBlock {
+    constructor(gameGrid) {
+        this.element = document.createElement("div");
+        this.element.classList.add("gridBlock");
+        this.taken = false;
+        this.active;
+        gameGrid.appendChild(this.element);
+
+        this.element.addEventListener("click", () => {
+            if (this.taken == false && addCube == true) {
+                addCube = false;
+                const div = document.getElementById("cubeHold");
+                div.style.opacity = 0.0
+
+                let newCube = new cube(1, 2000, 1);
+                this.element.appendChild(newCube.element);
+
+                this.active = setInterval(function () {
+                    newCube.element.style.transform = "scale(1.3)";
+                    newCube.element.style.opacity = "1.0";
+                    setTimeout(function () {
+                        newCube.element.style.transform = "scale(1.0)";
+                        newCube.element.style.opacity = "0.6";
+                    }, 150)
+                    money.add(newCube.value);
+                }, newCube.speed);
+
+                this.taken = true;
+            }
+            else if (removing == true) {
+                this.element.innerHTML = "";
+                clearInterval(this.active);
+                remove();
+            }
+            
+        });
+    }
+}
--- scripts.js
+++ scripts.js
@@ -1,16 +1,93 @@
-const gameGrid = document.getElementById("gameGrid");
+let money;
+let removing = false
+let addCube = false;
+let cubePrice = 25
 
-class gridBlock {
-    constructor(gameGrid) {
+class cube {
+    constructor(value, speed, level) {
+        this.value = value;
+        this.speed = speed;
+        this.level = level
         this.element = document.createElement("div");
-        this.element.classList.add("gridBlock");
-        gameGrid.appendChild(this.element);
+        this.element.classList.add("cube");
     }
 }
 
+class Money {
+    constructor(startingMoney) {
+        this.element = document.createElement("h1");
+        this.element.classList.add("money");
+        this.element.classList.add("ubuntu");
+        this.value = startingMoney;
+        this.element.innerHTML = "Money $" + this.value;
+        document.body.appendChild(this.element);
+    }
+
+    add(amount) {
+        this.value += amount
+        this.element.innerHTML = "Money $" + this.value;
+    }
+
+    reduce(amount) {
+        this.value -= amount
+        this.element.innerHTML = "Money $" + this.value;
+    }
+}
+
+function gainMoney(amount) {
+    money += amount;
+
+    let moneyTxt = document.getElementById("money");
+    moneyTxt.innerHTML = "Money: $" + money;
+}
+
+function loseMoney(amount) {
+    money -= amount;
+
+    let moneyTxt = document.getElementById("money");
+    moneyTxt.innerHTML = "Money: $" + money;
+}
+
 function fillGrid(blocks, rows) {
+    money = new Money(25)
     const gameGrid = document.getElementById("gameGrid");
     for (i=0; i<blocks; i++) {
         let Block = new gridBlock(gameGrid);
     }
 }
+
+function remove() {
+    let removeBtn = document.getElementById("remove");
+    if (removing == true) {
+        removing = false;
+        removeBtn.style.border = "4px solid grey";
+    }
+    else {
+        removing = true;
+        removeBtn.style.border = "5px solid coral";
+    }
+    
+}
+
+function buyCube() {
+    let buyBtn = document.getElementById("buyBtn");
+    if (money.value >= cubePrice) {
+        addCube = true;
+        money.reduce(cubePrice);
+        cubePrice = Math.round(cubePrice * 1.5);
+        buyBtn.innerHTML = "Buy Cube $" + cubePrice;
+    }
+}
+
+document.addEventListener("mousemove", (e) => {
+    const div = document.getElementById("cubeHold");
+
+    if (addCube == true) {
+        div.style.opacity = 1.0
+        div.style.left = (e.pageX - 25) + "px";
+        div.style.top = (e.pageY - 25) + "px";
+    }
+    else {
+        div.style.opacity = 0.0
+    }
+});
